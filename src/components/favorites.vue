0312c7346c7a6e1093bc9d1133426900d9a64912
Julianne Lee <julie@Juliannes-MacBook-Pro.local>
2019-11-20 17:16:20 -0700

    test commit 2

--- src/components/favorites.vue
+++ src/components/favorites.vue
@@ -41,7 +41,7 @@
                         <v-list-item-title>Born: {{ this.$store.state.favorites[1].personBirthYear }}</v-list-item-title>
                         <v-list-item-title>Gender: {{ this.$store.state.favorites[1].personGender }}</v-list-item-title>
                         <v-list-item-title>Eye Color: {{ this.$store.state.favorites[1].personEyeColor }}</v-list-item-title>
-                        <v-list-item-title>Height: {{ this.$store.state.favorites[1].personHeight }}cm</v-list-item-title>
+                        
                     </v-list-item-content>
                 </v-list-item>
         </v-card-text>
